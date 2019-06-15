008c8b4c7a7b731a985fd0b9a1290b4c1c95523e
Ian Haken <ihaken@netflix.com>
2019-06-14 17:31:00 -0700

    Use the libnotify library instead of shelling to notify-send.

--- notifier_linux.go
+++ notifier_linux.go
@@ -3,33 +3,54 @@
 
 package notifier
 
+/*
+// Generate flags with "pkg-config --cflags libnotify" and "pkg-config --libs libnotify"
+#cgo CFLAGS: -pthread -I/usr/include/gdk-pixbuf-2.0 -I/usr/include/libpng16 -I/usr/include/glib-2.0 -I/usr/lib/x86_64-linux-gnu/glib-2.0/include
+#cgo LDFLAGS: -lnotify -lgdk_pixbuf-2.0 -lgio-2.0 -lgobject-2.0 -lglib-2.0
+#include <libnotify/notify.h>
+*/
+import "C"
+
 import (
-	"fmt"
-	"os/exec"
+	"errors"
+	"os"
+	"unsafe"
 )
 
 type linuxNotifier struct{}
 
 // NewNotifier constructs notifier for Linux
 func NewNotifier() (Notifier, error) {
+	if C.notify_is_initted() == 0 {
+		appName := C.CString(os.Args[0])
+		defer C.free(unsafe.Pointer(appName))
+		res := C.notify_init(appName)
+		if res == 0 {
+			return nil, errors.New("Unable to run notify_init.")
+		}
+	}
 	return &linuxNotifier{}, nil
 }
 
 // DeliverNotification sends a notification
 func (n linuxNotifier) DeliverNotification(notification Notification) error {
-	args := []string{}
-	if notification.ImagePath != "" {
-		args = append(args, "-i", notification.ImagePath)
+	title := C.CString(notification.Title)
+	defer C.free(unsafe.Pointer(title))
+	body := C.CString(notification.Message)
+	defer C.free(unsafe.Pointer(body))
+	imagePath := C.CString(notification.ImagePath)
+	defer C.free(unsafe.Pointer(imagePath))
+
+	note := C.notify_notification_new(title, body, imagePath)
+	defer C.g_object_unref(C.gpointer(note))
+
+	var gerr *C.GError
+	res := C.notify_notification_show(note, &gerr)
+	if res == 0 {
+		msg := C.GoString(gerr.message)
+		C.g_error_free(gerr)
+		return errors.New(msg)
 	}
-	args = append(args, notification.Title)
-	args = append(args, notification.Message)
-	cmd := exec.Command("notify-send", args...)
-	if cmd == nil {
-		return fmt.Errorf("No command")
-	}
-	_, err := cmd.Output()
-	if err != nil {
-		return fmt.Errorf("Error running command: %s", err)
-	}
+
 	return nil
 }
